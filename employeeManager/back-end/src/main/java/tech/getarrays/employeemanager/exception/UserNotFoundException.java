fc6eff529eeadfc97a1a04bc642e526a8e763ae3
ibrambit <ibrambit@ibrambits-MacBook-Pro.local>
2022-09-28 00:00:31 +0300

    first commit

---
+++ employeeManager/back-end/src/main/java/tech/getarrays/employeemanager/exception/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package ibram.employeeManager.exception;
+
+public class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
